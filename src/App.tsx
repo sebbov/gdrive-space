5c971147972afff90b7cb6924b8bba13d4bd13b1
Sebastien Boving <github@sebastienboving.com>
2024-12-18 10:36:38 -0700

    Setup tailwind for styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Drive-by-fix</h1>
+      <h1 className="text-3xl font-bold text-center text-blue-600">
+        Drive-by-fix
+      </h1>
     </>
   )
 }
